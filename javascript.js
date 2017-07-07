e507a7958a190e2311ad44901cd657eb59a9422a
Mindaugas Kuzminskas <mindkuzm@gmail.com>
2017-07-07 15:02:18 +0300

    +gallery

--- javascript.js
+++ javascript.js
@@ -1,5 +1,31 @@
 $(document).ready(function() {
+	$('.container').slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		centerMode: true,
+		arrows: true,
+	});
+
+	var ID_ALBUM = '993671707412628'
+	var ACCESS_TOKEN = 'EAAUmIopVGCsBABmBrSSy9BJ4sQOfZCLphPcLlrDAPx08yonAi2r6cZCGZC6TBE8joVtEX3Pfhi3vEVLLSGNietxfnRtjTkugN9ZBHyDBFfqr6UrsW4xzxZBqw89xWivQ42tCuL2Wl3jSserMGPacn6JCHG9QkVFJkZBqLmKoSSxAZDZD';
+	var GALLERY = "https://graph.facebook.com/"+ID_ALBUM+"/photos?fields=images&limit=80&access_token="+ACCESS_TOKEN;
+
+	$.ajax({
+		url: GALLERY,
+		method: 'GET',
+		contentType: 'application/json',
+		success: celebrate
+	});
+
+	function celebrate(response){
+		for (i=0; i<Object.keys(response.data).length; i++)	{
+			var url = response.data[i].images[8].source; 
+			$('.container').slickAdd('<div class="item"><img src="'+url+'" /></div>');
+		}
+	}
+
 });
+
 function initMap() {
 	var uluru = {lat: 54.911680, lng: 23.937070};
 	var map = new google.maps.Map(document.getElementById('map'), {
